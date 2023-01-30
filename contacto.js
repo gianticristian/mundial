aebc42b3d7e408282d4e6807e460a89b57ad9ca9
Cristian Gianti <gianticristian@gmail.com>
2023-01-30 17:01:36 -0300

    Boton de Wapp, formulario, y correo

---
+++ contacto.js
@@ -0,0 +1,9 @@
+function mostrarFormulario () {
+    document.getElementById("correo").style.display = "none";
+    document.getElementById("formulario").style.display = "block";
+}
+
+function mostrarCorreo () {
+    document.getElementById("formulario").style.display = "none";
+    document.getElementById("correo").style.display = "block";
+}
